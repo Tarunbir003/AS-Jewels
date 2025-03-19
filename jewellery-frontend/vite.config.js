aaf8790d15af1832ea5e0560587d8274cd07f5f4
Bir <140426404+Tarunbir003@users.noreply.github.com>
2025-03-19 15:32:28 +0530

    Update vite.config.js

--- jewellery-frontend/vite.config.js
+++ jewellery-frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
