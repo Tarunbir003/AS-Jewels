6d0dd9c2715e16a6fa3b43edeb659476b983dcb8
Bir <140426404+Tarunbir003@users.noreply.github.com>
2025-04-10 12:41:38 -0600

    Update Register.jsx

--- jewellery-frontend/src/components/Auth/Register.jsx
+++ jewellery-frontend/src/components/Auth/Register.jsx
@@ -41,15 +41,16 @@ const Register = () => {
 <App_info/>
 <div className="flex min-h-screen items-center bg-white min-w-8 max-w-96 flex-1 flex-col justify-center px-6 py-12 lg:px-8 scale-99">
 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-   <div className="flex justify-center items-center mb-6">
+  <div className="flex justify-center items-center mt-6 mb-10">
   <a href="/">
     <img 
-      className="h-28 w-auto drop-shadow-xl" 
+      className="h-32 w-auto drop-shadow-xl hover:scale-105 transition-transform duration-300" 
       src="/logo.png" 
-      alt="AS Jewels Logo"
+      alt="AS Jewels Logo" 
     />
   </a>
 </div>
+
 
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Create your account
