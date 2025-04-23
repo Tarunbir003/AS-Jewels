0bd7ce17d0b1ac81ff25e9198277bbbab296b056
Bir <140426404+Tarunbir003@users.noreply.github.com>
2025-04-23 12:09:38 -0600

    Update Products.jsx

--- jewellery-frontend/src/components/App/Products.jsx
+++ jewellery-frontend/src/components/App/Products.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import Header from "./Header";
 import Cart from "./Cart";
 import Toast from "./Toast";
 import Select from "react-select";
